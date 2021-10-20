4ab08765420d03bab9e0d8ff0955b86e11619379
Leandro <leandr12eu@gmail.com>
2021-10-20 21:10:23 -0200

    first commit

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import model.Gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+        /* int a = 5;
+        int b = 3;
+        System.out.println("Hello Word!" + (a+b));*/
+    }
+
+}
